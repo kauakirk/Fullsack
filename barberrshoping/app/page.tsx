055e406cdccb444ad4cc1184727c0298f5f0fd03
kaua kirk <103045211+kauakirk@users.noreply.github.com>
2025-12-22 20:13:31 +0000

    feat: configuracoes iniciais

---
+++ barberrshoping/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <Button variant={"destructive"}> Enviar </Button>
+      <Input type="text" placeholder="Enter your name" />
+    </div>
+  );
+}
